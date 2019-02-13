fb9a0328d09442e86444423722cd08054f0015ea
nitigan <nitigan.n@fitm.kmutnb.ac.th>
2019-02-13 15:29:09 +0700

    ATM

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    ATM Login<br>
+    Username : <input type="text"><br>
+    Password : <input type="password"><br>
+    <input type="submit">
+    <input type="reset">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home'
+}
+</script>
