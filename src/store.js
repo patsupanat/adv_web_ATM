4f06dcb685a01228e7dc2ac4260052190dbec77e
nitigan <nitigan.n@fitm.kmutnb.ac.th>
2019-02-15 12:38:35 +0700

    15FEB2019

--- src/store.js
+++ src/store.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    users:
+      [
+        {
+          username:'admin',
+          password:'admin'
+        },
+        {
+          username:'user',
+          password:'user'
+        }
+      ],
+      userLogin:{}
   },
   mutations: {
-
+    SET_USER_LOGIN(state,payload){
+      state.userLogin=payload
+    }
   },
   actions: {
-
+    setUserLogin({commit},payload){
+      commit('SET_USER_LOGIN',payload)
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="home">
     ATM Login<br>
-    Username : <input type="text"><br>
-    Password : <input type="password"><br>
-    <input type="submit">
-    <input type="reset">
+    Username : <input type="text" v-model="loginUser.username"><br>
+    Password : <input type="password" v-model="loginUser.password"><br>
+    <input type="submit" @click="login()">
+
 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
+import {mapState,mapActions} from 'vuex'
 export default {
-  name: 'home'
+  name: 'home',
+  data(){
+    return{
+      loginUser:{
+        username:'',
+        password:''
+      }
+    }
+  },
+  methods:{
+    login(){
+      let index = this.users.findIndex(user => user.username === this.loginUser.username &&
+      user.password === this.loginUser.password)
+      if(index === -1){
+        alert('Username or Password ไม่ถูกต้อง!!')
+      }else{
+        this.setUserLogin(this.loginUser)
+        this.$router.push({name:'about'})
+      }
+    },
+    ...mapActions({
+      setUserLogin:'setUserLogin'
+    })
+  },
+  computed:{
+    ...mapState({
+      users:state=>state.users
+    })
+  },
+
 }
 </script>
